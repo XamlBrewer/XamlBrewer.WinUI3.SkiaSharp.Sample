1d3787272fda284b89ebffca517ab7e9cc80b6bd
Diederik Krols <diederik_krols@hotmail.com>
2023-08-01 16:47:35 +0200

    Hello World

--- XamlBrewer.WinUI3.SkiaSharp.Sample/Views/HomePage.xaml.cs
+++ XamlBrewer.WinUI3.SkiaSharp.Sample/Views/HomePage.xaml.cs
@@ -2,7 +2,8 @@ using Microsoft.UI.Xaml.Controls;
 using SkiaSharp;
 using SkiaSharp.Views.Windows;
 using System.IO;
-using System.Reflection;
+using Windows.ApplicationModel;
+using SKSvg = SkiaSharp.Extended.Svg.SKSvg;
 
 namespace XamlBrewer.WinUI3.SkiaSharp.Sample
 {
@@ -15,51 +16,40 @@ namespace XamlBrewer.WinUI3.SkiaSharp.Sample
 
         private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
-            // https://github.com/mono/SkiaSharp/tree/13b699c9fcb4e2c2690cb6327e4b6a59c44b4a57/samples/Gallery
-
             SKCanvas canvas = e.Surface.Canvas;
 
-            canvas.Clear(SKColors.White);
+            canvas.Clear(SKColors.Transparent);
 
-            // set up drawing tools
+            // Read and draw an SVG file
+            var svg = new SKSvg();
+            svg.Load(Path.Combine(Package.Current.InstalledLocation.Path, "Assets/WinUI_Logo.svg"));
+            var coord = new SKPoint(80, (e.Info.Height - svg.ViewBox.Height) / 2); // Origin = Top Left
+            //var coord = new SKPoint(80, (e.Info.Height - svg.Picture.CullRect.Height) / 2);
+            canvas.DrawPicture(svg.Picture, coord);
+
+            // Draw some text
+            string text = "SkiaSharp on WinUI";
             var paint = new SKPaint
             {
+                Color = SKColors.Gold,
                 IsAntialias = true,
-                Color = new SKColor(0x2c, 0x3e, 0x50),
-                StrokeCap = SKStrokeCap.Round
-            };
-
-            // create the Xamagon path
-            var path = new SKPath();
-            path.MoveTo(71.4311121f, 56f);
-            path.CubicTo(68.6763107f, 56.0058575f, 65.9796704f, 57.5737917f, 64.5928855f, 59.965729f);
-            path.LineTo(43.0238921f, 97.5342563f);
-            path.CubicTo(41.6587026f, 99.9325978f, 41.6587026f, 103.067402f, 43.0238921f, 105.465744f);
-            path.LineTo(64.5928855f, 143.034271f);
-            path.CubicTo(65.9798162f, 145.426228f, 68.6763107f, 146.994582f, 71.4311121f, 147f);
-            path.LineTo(114.568946f, 147f);
-            path.CubicTo(117.323748f, 146.994143f, 120.020241f, 145.426228f, 121.407172f, 143.034271f);
-            path.LineTo(142.976161f, 105.465744f);
-            path.CubicTo(144.34135f, 103.067402f, 144.341209f, 99.9325978f, 142.976161f, 97.5342563f);
-            path.LineTo(121.407172f, 59.965729f);
-            path.CubicTo(120.020241f, 57.5737917f, 117.323748f, 56.0054182f, 114.568946f, 56f);
-            path.LineTo(71.4311121f, 56f);
-            path.Close();
-
-            // draw the Xamagon path
-            canvas.DrawPath(path, paint);
-
-            // draw some text
-            var paint2 = new SKPaint
-            {
-                Color = SKColors.Black,
-                IsAntialias = true,
-                Style = SKPaintStyle.Fill,
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = 2,
                 TextAlign = SKTextAlign.Center,
-                TextSize = 24
+                TextSize = 58
             };
-            var coord = new SKPoint(e.Info.Width / 2, (e.Info.Height + paint2.TextSize) / 2);
-            canvas.DrawText("SkiaSharp", coord, paint2);
+            var bounds = new SKRect();
+            paint.MeasureText(text, ref bounds);
+            coord = new SKPoint(e.Info.Width / 2, (e.Info.Height + bounds.Height) / 2 ); // Origin = Bottom Left
+            canvas.DrawText(text, coord, paint);
+
+            // Overlapping transparent SVG sample
+            //svg = new SKSvg();
+            //svg.Load(SampleMedia.Images.OpacitySvg);
+            //coord = new SKPoint(e.Info.Width - svg.Picture.CullRect.Width, 0);
+            //canvas.DrawPicture(svg.Picture, coord);
+            //coord = new SKPoint(e.Info.Width - svg.Picture.CullRect.Width, e.Info.Height - svg.Picture.CullRect.Height);
+            //canvas.DrawPicture(svg.Picture, coord);
         }
     }
 }
